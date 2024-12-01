e4265b55ce33c258761df77e5d04608472cfb5ec
trytofixme <semenov.nikita@phystech.edu>
2024-12-01 21:26:36 +0300

    Initial commit

---
+++ AreaCalculator.Tests/CircleAreaTest.cs
@@ -0,0 +1,48 @@
+﻿using AreaCalculator.Figures;
+using System.Collections;
+
+namespace AreaCalculator.Tests;
+
+[TestFixture]
+public class CircleAreaTest
+{
+    private FigureFactory? _figureFactory;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _figureFactory = new FigureFactory();
+    }
+
+    [TestCaseSource(typeof(CircleTestCases), nameof(CircleTestCases.CircleInvalidCases))]    
+    public void CircleVidatationTest(double radius)
+    {       
+        Assert.That(() => _figureFactory!.CreateCircle(radius), Throws.TypeOf<ArgumentException>());
+    }
+
+    [TestCaseSource(typeof(CircleTestCases), nameof(CircleTestCases.CircleCases))]
+    public double CircleAreaCalculatorTest(double radius)
+    {
+        var circle = _figureFactory!.CreateCircle(radius);
+        return circle.GetArea();
+    }
+}
+
+public class CircleTestCases
+{
+    public static IEnumerable CircleInvalidCases
+    {
+        get
+        {
+            yield return new TestCaseData(-1);
+        }
+    }
+
+    public static IEnumerable CircleCases
+    {
+        get
+        {
+            yield return new TestCaseData(1).Returns(Math.PI);
+        }
+    }
+}
---
+++ AreaCalculator.Tests/TriangleAreaTest.cs
@@ -0,0 +1,70 @@
+﻿using AreaCalculator.Figures;
+using System.Collections;
+using AreaCalculator.Figures.Model;
+
+namespace AreaCalculator.Tests;
+
+[TestFixture]
+public class TriangleAreaTest
+{
+    private FigureFactory? _figureFactory;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _figureFactory = new FigureFactory();
+    }
+
+    [TestCaseSource(typeof(TriangleTestCases), nameof(TriangleTestCases.TriangleInvalidCases))]
+    public void TriangleVidatationTest(double sideA, double sideB, double sideC)
+    {
+        Assert.That(() => _figureFactory!.CreateTriangle(sideA, sideB, sideC), Throws.TypeOf<ArgumentException>());
+    }
+
+    [TestCaseSource(typeof(TriangleTestCases), nameof(TriangleTestCases.TriangleCases))]
+    public double TriangleAreaCalculatorTest(double sideA, double sideB, double sideC)
+    {
+        var triangle = _figureFactory!.CreateTriangle(sideA, sideB, sideC);
+        return triangle.GetArea();
+    }
+
+    [TestCaseSource(typeof(TriangleTestCases), nameof(TriangleTestCases.RectangularTriangleCases))]
+    public bool RectangularTriangleTest(double sideA, double sideB, double sideC)
+    {
+        var triangle = _figureFactory!.CreateTriangle(sideA, sideB, sideC) as Triangle;
+        return triangle!.IsTriangleRectangular();
+    }
+
+    public class TriangleTestCases
+    {
+        public static IEnumerable TriangleInvalidCases
+        {
+            get
+            {
+                yield return new TestCaseData(-1, 2, 3);
+                yield return new TestCaseData(1, -2, 3);
+                yield return new TestCaseData(1, 2, -3);
+                yield return new TestCaseData(5, 1, 1);
+                yield return new TestCaseData(1, 2, 4);
+            }
+        }
+
+        public static IEnumerable TriangleCases
+        {
+            get
+            {
+                yield return new TestCaseData(5, 3, 4).Returns(6);
+                yield return new TestCaseData(13, 13, 10).Returns(60);
+            }
+        }
+
+        public static IEnumerable RectangularTriangleCases
+        {
+            get
+            {
+                yield return new TestCaseData(3, 4, 5).Returns(true);
+                yield return new TestCaseData(13, 13, 10).Returns(false);
+            }
+        }
+    }
+}
